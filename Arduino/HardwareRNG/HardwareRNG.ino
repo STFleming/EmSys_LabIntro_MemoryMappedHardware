da01fd0da6a23f6841ba0b9b76b4f826ade6f875
Shane T. Fleming <t-shflem@microsoft.com>
2021-02-01 23:40:23 +0000

    Added a Hardware random number generator example

---
+++ Arduino/HardwareRNG/HardwareRNG.ino
@@ -0,0 +1,17 @@
+// Setting up the serial connection
+// author: stf
+
+unsigned int *hardware_rng; // a variable that is a pointer
+
+void setup() {
+        Serial.begin(115200);
+        Serial.print("\n\n");
+
+        hardware_rng = (unsigned int*)(0x3FF75144);
+        
+}
+
+void loop() {
+    delay(1);
+    Serial.println(*hardware_rng);
+}
