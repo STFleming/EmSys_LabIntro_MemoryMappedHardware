9eacdc75fc1a7d975b730c3eed903e0725e70dbb
Shane T. Fleming <t-shflem@microsoft.com>
2021-02-02 05:56:30 +0000

    triple pointers

--- Arduino/DoublePointer/DoublePointer.ino
+++ Arduino/DoublePointer/DoublePointer.ino
@@ -4,6 +4,7 @@
 int a; // a variable
 int *ptr_a; // a variable that is a pointer
 int **ptr_ptr_a;
+int ***ptr_ptr_ptr_a;
 
 void setup() {
         Serial.begin(115200);
@@ -13,6 +14,7 @@ void setup() {
         a = 10;
         ptr_a = &a; // ptr is pointing to the address of a
         ptr_ptr_a = &ptr_a; // a pointer to a pointer of a
+        ptr_ptr_ptr_a = &ptr_ptr_a;
 
         Serial.print("Variable a has value:");
         Serial.println(a);
@@ -21,7 +23,7 @@ void setup() {
         Serial.println((unsigned int)ptr_a, HEX);
 
         Serial.print("Double dereference: ");
-        Serial.println(**ptr_ptr_a);
+        Serial.println(***ptr_ptr_ptr_a);
 }
 
 void loop() {
