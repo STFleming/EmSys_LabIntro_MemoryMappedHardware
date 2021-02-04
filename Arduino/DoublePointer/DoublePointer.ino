0338c452121a249f57368a554a7e1033a0812112
Shane T. Fleming <t-shflem@microsoft.com>
2021-02-04 00:08:42 +0000

    Woops overwrote this at some point

--- Arduino/DoublePointer/DoublePointer.ino
+++ Arduino/DoublePointer/DoublePointer.ino
@@ -1,15 +1,25 @@
 // author: stf
 
+int a; // a variable
+int *ptr_a; // a variable that is a pointer
+int **ptr_ptr_a;
+int ***ptr_ptr_ptr_a;
+
 void setup() {
         Serial.begin(115200);
-        Serial.print("\n\n");
-  
-        Serial.print("datatype      size\n");
-        Serial.print("int           "); Serial.println(sizeof(int));
-        Serial.print("int           "); Serial.println(sizeof(float));
-        Serial.print("int           "); Serial.println(sizeof(char));
-        Serial.print("int           "); Serial.println(sizeof(uint16_t));
-        Serial.print("int           "); Serial.println(sizeof(bool));
+        a = 10;
+        ptr_a = &a; // ptr is pointing to the address of a
+        ptr_ptr_a = &ptr_a; // a pointer to a pointer of a
+        ptr_ptr_ptr_a = &ptr_ptr_a;
+
+        Serial.print("Variable a has value:");
+        Serial.println(a);
+        Serial.println((unsigned int)ptr_a, HEX);
+
+        Serial.print("Double dereference: ");
+        Serial.println(**ptr_ptr_a);
+        Serial.print("Triple dereference: ");
+        Serial.println(***ptr_ptr_ptr_a);
 }
 
 void loop() {
